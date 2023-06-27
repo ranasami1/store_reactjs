353e4f5eee519f954d6cac9af12cf963437c3698
rana sami rezk <104070983+ranasami1@users.noreply.github.com>
2023-06-27 13:30:40 +0300

    adding filter buttons

--- src/pages/allproducts/products.js
+++ src/pages/allproducts/products.js
@@ -2,6 +2,7 @@ import React from "react";
 import PCard from "../../component/cardC/card";
 import { useEffect,useState } from "react";
 import "./products.css"
+import { Button } from "react-bootstrap";
 const Allproducts = () =>{
     const  [value,setValue] = useState([]);
     const url ="https://fakestoreapi.com/products"
@@ -14,6 +15,14 @@ const Allproducts = () =>{
     <div className="container prod">
         <div className="row">
         <h1 className="text-center">Our Products</h1>
+        <div className="filter">
+            <p>Filter-_</p>
+        <Button id="filterb"variant="outline-secondary">all products</Button>
+        <Button id="filterb"variant="outline-secondary">Men</Button>
+        <Button id="filterb"variant="outline-secondary">Women</Button>
+        <Button id="filterb"variant="outline-secondary">Jewelery</Button>
+        <Button id="filterb"variant="outline-secondary">Electronics</Button></div>
+        
                     {value.map((card) =>{
                         return(
                             <div className="col-3" key={value.id}>
