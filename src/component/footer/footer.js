adc939c843005ae6cd1b1e4fb2f2712b04e1950c
ranasami1 <ranasami237@gmail.com>
2024-05-15 20:47:24 +0300

    fixing responsive error

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -3,20 +3,20 @@ import React from "react";
 import logo from "./../../img/pngwing.com (1).png"
 const Footer = ()=>{
     return(
-        <footer>
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-3">
+        <footer className="w-100">
+        <div>
+            <div className="d-flex align-items-center justify-content-around gap-5 flex-wrap">
+                <div>
                     <div className="footer-content">
                         <a href="#" className="logo" >
                             <img src={logo} style={{width:30}} alt=""/>
                         </a>
-                        <p className="text-gray">
+                        <p className="text-gray" style={{maxWidth:"300px"}}>
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, doloribus sit deleniti asperiores officiis odit animi, labore laboriosam sunt deserunt dolores molestias accusantium eius fugiat adipisci et repellendus nihil neque.
                         </p>
                     </div>
                 </div>
-                <div className="col-3">
+                <div>
                     <div className="footer-content">
                         <div className="footer-content-header">
                             <h5>Shop</h5>
@@ -40,7 +40,7 @@ const Footer = ()=>{
                         </ul>
                     </div>
                 </div>
-                <div className="col-3">
+                <div>
                     <div className="footer-content">
                         <div className="footer-content-header">
                         <h5>Information</h5>
@@ -64,7 +64,7 @@ const Footer = ()=>{
                         </ul>
                     </div>
                 </div>
-                <div className="col-3">
+                <div>
                     <div className="footer-content">
                         <div className="footer-content-header">
                         <h5>Policies</h5>
